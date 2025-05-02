378f0f090e7e2031093a1c847761d66dcc4657bc
KingZord263 <kingzord263@gmail.com>
2025-05-02 09:32:51 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
+# SUHAIL BRO SESSION GENERATOR 
+
+
+
+
+> **A simple and elegant way to generate pairing codes for bots with Malvin Baileys using mega.js. Easily deployable and ready for integration into your bot system.**
+---
+
+<p align="center">
+   <a href="https://github.com/KingZord263">
+    <img src="https://i.imgur.com/teQzVR1.jpeg" width="500">
+   </a>
+</p>
+
+<p align="center">
+   <img src="https://profile-counter.glitch.me/{XdKing2}/count.svg" alt="Malvin Xd:: Visitor's Count" />
+</p>
+
+---
+
+### **Fork & Contribute**
+Feel free to **Fork** or **Star** this repository to contribute or personalize it.
+
+<p align="center">
+  <a href="https://github.com/KingZord263/Vannessa/fork">
+    <img alt="Fork this repo" src="https://img.shields.io/badge/Fork%20This%20Repo-black?style=for-the-badge&logo=github&logoColor=white" />
+  </a>
+  <a href="https://github.com/KingZord263/PATRON-MDS/stargazers">
+    <img alt="Star this repo" src="https://img.shields.io/github/stars/Kingzord263/GOTAR-MD-SESSION?style=for-the-badge&logo=github&logoColor=white" />
+  </a>
+</p>
+
+---
+
+### **Quick Deployment**
+Deploy the project to your favorite hosting platform with the buttons below:
+
+- **Deploy to Heroku**
+  <p align="center">
+    <a href="https://dashboard.heroku.com/new?template=https://github.com/KINGZORD263/GOTAR-MD-SESSION" target="_blank">
+      <img alt="Deploy to Heroku" src="https://img.shields.io/badge/Deploy%20to%20Heroku-black?style=for-the-badge&logo=heroku&logoColor=white" />
+    </a>
+  </p>
+
+- **Deploy to Render**
+  <p align="center">
+    <a href="https://dashboard.render.com" target="_blank">
+      <img alt="Deploy to Render" src="https://img.shields.io/badge/Deploy%20to%20Render-black?style=for-the-badge&logo=render&logoColor=white" />
+    </a>
+  </p>
+
+- **Deploy to Koyeb**
+  <p align="center">
+    <a href="https://app.koyeb.com" target="_blank">
+      <img alt="Deploy to Koyeb" src="https://img.shields.io/badge/Deploy%20to%20Koyeb-black?style=for-the-badge&logo=koyeb&logoColor=white" />
+    </a>
+  </p>
+
+---
+
+![BACKGROUNDIMAGE](https://files.catbox.moe/6f05ah.jpg)
+ [CONTACT DEV](https://wa.me/263776404156)
+ 
 
---
+++ qr.js
@@ -0,0 +1,165 @@
+const { makeid } = require('./gen-id');
+const express = require('express');
+const QRCode = require('qrcode');
+const fs = require('fs');
+let router = express.Router();
+const pino = require("pino");
+const {
+    default: makeWASocket,
+    useMultiFileAuthState,
+    delay,
+    makeCacheableSignalKeyStore,
+    Browsers,
+    jidNormalizedUser
+} = require("@whiskeysockets/baileys");
+const { upload } = require('./mega');
+function removeFile(FilePath) {
+    if (!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true });
+}
+router.get('/', async (req, res) => {
+    const id = makeid();
+ //   let num = req.query.number;
+    async function GIFTED_MD_PAIR_CODE() {
+        const {
+            state,
+            saveCreds
+        } = await useMultiFileAuthState('./temp/' + id);
+        try {
+var items = ["Safari"];
+function selectRandomItem(array) {
+  var randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
+var randomItem = selectRandomItem(items);
+            
+            let sock = makeWASocket({
+                	
+				auth: state,
+				printQRInTerminal: false,
+				logger: pino({
+					level: "silent"
+				}),
+				browser: Browsers.macOS("Desktop"),
+			});
+            
+            sock.ev.on('creds.update', saveCreds);
+            sock.ev.on("connection.update", async (s) => {
+                const {
+                    connection,
+                    lastDisconnect,
+                    qr
+                } = s;
+              if (qr) await res.end(await QRCode.toBuffer(qr));
+                if (connection == "open") {
+                    await delay(5000);
+                    let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                    let rf = __dirname + `/temp/${id}/creds.json`;
+                    function generateRandomText() {
+                        const prefix = "3EB";
+                        const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                        let randomText = prefix;
+                        for (let i = prefix.length; i < 22; i++) {
+                            const randomIndex = Math.floor(Math.random() * characters.length);
+                            randomText += characters.charAt(randomIndex);
+                        }
+                        return randomText;
+                    }
+                    const randomText = generateRandomText();
+                    try {
+                        const { upload } = require('./mega');
+                        const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
+                        const string_session = mega_url.replace('https://mega.nz/file/', '');
+                        let md = "PARSLEY-XMD~" + string_session;
+                        let code = await sock.sendMessage(sock.user.id, { text: md });
+                        let desc = `*Hello  1.0.0 User! 👋🏻* 
+
+> 🚨 *CONFIDENTIALITY ALERT!* 🚨
+
+Keep your session ID under wraps! Don't share it with anyone.
+
+Thanks for choosing PARSLEY-XMD 1.0.0 🚩!
+
+*Ready to level up?* Fork our repo ⬇️ [https://github.com/KINGZORD263/PARSLEY-XMD] and contribute to the community!*
+
+*You're the missing piece!* Help us build something amazing. Fork the repo now!
+
+*Stay ahead of the game!* Join our channel for the latest updates and behind-the-scenes insights.
+
+You're part of something awesome!"
+
+> *© SUHAIL BRU*`; 
+                        await sock.sendMessage(sock.user.id, {
+text: desc,
+contextInfo: {
+externalAdReply: {
+title: " 𝖇𝖔𝖙 𝖈𝖔𝖓𝖓𝖊𝖈𝖙𝖊𝖉",
+thumbnailUrl: "https://files.catbox.moe/3bwori.jpg",
+sourceUrl: "https://whatsapp.com/channel/0029VbAJA1THwXbA74a5pO1s",
+mediaType: 1,
+renderLargerThumbnail: true
+}  
+}
+},
+{quoted:code })
+                    } catch (e) {
+                            let ddd = sock.sendMessage(sock.user.id, { text: e });
+                            let desc = `*Hello PARSLEY-XMD 1.0.0 User! 👋🏻* 
+                            
+> 🚨 *CONFIDENTIALITY ALERT!* 🚨
+
+Keep your session ID under wraps! Don't share it with anyone.
+
+Thanks for choosing PARSLEY-XMD 1.0.0 🚩!
+
+
+*We've got a secret for you...* 🤫 Join our channel to unlock exclusive content
+
+*You're the missing piece!* Help us build something amazing. Fork the repo now!
+
+*Stay ahead of the game!* Join our channel for the latest updates and behind-the-scenes insights.
+
+You're part of something awesome!"
+
+> *©SUHAIL BRU*`; 
+                            await sock.sendMessage(sock.user.id, {
+text: desc,
+contextInfo: {
+externalAdReply: {
+title: " 𝖒𝖉 𝖈𝖔𝖓𝖓𝖊𝖈𝖙𝖊𝖉 ✅  ",
+thumbnailUrl: "https://files.catbox.moe/rvm1iz.jpg",
+sourceUrl: "https://whatsapp.com/channel/0029VbAJA1THwXbA74a5pO1s",
+mediaType: 2,
+renderLargerThumbnail: true,
+showAdAttribution: true
+}  
+}
+},
+{quoted:ddd })
+                    }
+                    await delay(10);
+                    await sock.ws.close();
+                    await removeFile('./temp/' + id);
+                    console.log(`👤 ${sock.user.id} 𝗖𝗼𝗻𝗻𝗲𝗰𝘁𝗲𝗱 ✅ 𝗥𝗲𝘀𝘁𝗮𝗿𝘁𝗶𝗻𝗴 𝗽𝗿𝗼𝗰𝗲𝘀𝘀...`);
+                    await delay(10);
+                    process.exit();
+                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10);
+                    GIFTED_MD_PAIR_CODE();
+                }
+            });
+        } catch (err) {
+            console.log("service restated");
+            await removeFile('./temp/' + id);
+            if (!res.headersSent) {
+                await res.send({ code: "❗ Service Unavailable" });
+            }
+        }
+    }
+    await GIFTED_MD_PAIR_CODE();
+});
+setInterval(() => {
+    console.log("☘️ 𝗥𝗲𝘀𝘁𝗮𝗿𝘁𝗶𝗻𝗴 𝗽𝗿𝗼𝗰𝗲𝘀𝘀...");
+    process.exit();
+}, 180000); //30min
+module.exports = router;
